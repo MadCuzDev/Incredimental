aeced1e2b97f6bee1709e03eb705dac8e00760bc
MadCuzDev <30938682+MadCuzDev@users.noreply.github.com>
2023-10-27 10:41:58 -0400

    Added max prestige Fixed blocksPerSecond not saving Tidied up UI (especially for 1080p) Changed reset to ClearStorage & reload Added explosive and cubic enchantments

--- main.js
+++ main.js
@@ -11,18 +11,33 @@ let autoBlocksPerSecond = 0;
 let autoBlockValuePerSecond = 0;
 let fortune = 0;
 let tokenFinder = 0;
+let explosive = 0;
+let cubic = 0;
+let maxPrestige = false;
+let maxPrestigeUnlocked = false;
+
+let currentTick = 1;
+
+let averageMoneyPerSec = 0;
+let averageTokensPerSec = 0;
+
+let displayMoneyPerSec = 0;
+let displayTokensPerSec = 0;
+
+let doNotSave = false;
+
 
 function update() {
     // Update money display
     const moneyDisplay = document.getElementById("money");
-    let moneyDisplayText = `$${money.toFixed(2)}`;
-    moneyDisplayText+=`\r\nTokens ${tokens.toFixed(0)}`;
-    moneyDisplayText+=`\r\nTokens/Second ${(blocksPerSecond * getTokenMulti()).toFixed(1)}`;
-    moneyDisplayText+=`\r\nBlocks/Second ${blocksPerSecond.toFixed(1)}`;
-    moneyDisplayText+=`\r\n$/Block ${(moneyPerBlock * getMoneyMulti()).toFixed(1)}`;
-    moneyDisplayText+=`\r\n$/Second ${(blocksPerSecond * (moneyPerBlock * getMoneyMulti())).toFixed(0)}`;
-    moneyDisplayText+=`\r\nPrestige ${prestige}`;
-    moneyDisplayText+=`\r\nPrestige Points ${prestigePoints}`;
+    let moneyDisplayText = `$${formatNumber(money)}`;
+    moneyDisplayText+=`\r\n$/Second ${formatNumber(displayMoneyPerSec)}\r\n`;
+    moneyDisplayText+=`\r\nTokens ${formatNumber(tokens, 0)}`;
+    moneyDisplayText+=`\r\nTokens/Second ${formatNumber(displayTokensPerSec, 0)}\r\n`;
+    moneyDisplayText+=`\r\nBlocks/Second ${formatNumber(blocksPerSecond, 1)}`;
+    moneyDisplayText+=`\r\n$/Block ${formatNumber(moneyPerBlock * getMoneyMulti())}\r\n`;
+    moneyDisplayText+=`\r\nPrestige ${formatNumber(prestige, 0)}`;
+    moneyDisplayText+=`\r\nPrestige Points ${prestigePoints}\r\n`;
     moneyDisplayText+=`\r\nMastery ${mastery}`;
     moneyDisplayText+=`\r\nMastery Points ${masteryPoints}`;
 
@@ -32,6 +47,9 @@ function update() {
     const enchantmentDisplay = document.getElementById("enchantmentsDisplay");
     let enchantmentsDisplayText = `Fortune: ${fortune}`;
     enchantmentsDisplayText+=`\r\nToken Finder: ${tokenFinder}`;
+    enchantmentsDisplayText+=`\r\nExplosive: ${explosive}`;
+    enchantmentsDisplayText+=`\r\nCubic: ${cubic}`;
+
 
     enchantmentDisplay.textContent = enchantmentsDisplayText;
 
@@ -39,15 +57,23 @@ function update() {
     const prestigeDisplay = document.getElementById("prestigeDisplay");
     let prestigeDisplayText = `auto blocks/sec: ${autoBlocksPerSecond.toFixed(1)}`;
     prestigeDisplayText+=`\r\n auto block value/sec: ${autoBlockValuePerSecond.toFixed(1)}`
+    prestigeDisplayText+=`\r\n max prestige button: ${maxPrestigeUnlocked ? "Unlocked" : "Locked"}`
 
     prestigeDisplay.textContent = prestigeDisplayText;
 
     // Update prestige & mastery button
     const prestigeButton = document.getElementById("prestigeButton");
-    prestigeButton.textContent = `Prestige for ${prestige+2}x money\r\nCost: ${getPrestigeCost(prestige)}`;
+    if (maxPrestige) {
+        prestigeButton.textContent = `Prestige for ${getMaxPrestige()[1]+1}x money\r\nCost: $${formatNumber(getMaxPrestige()[0])}`;
+    } else {
+        prestigeButton.textContent = `Prestige for ${prestige+2}x money\r\nCost: $${formatNumber(getPrestigeCost(prestige))}`;
+    }
 
     const masteryButton = document.getElementById("masteryButton");
     masteryButton.textContent = `Mastery for ${mastery+2}x tokens\r\nCost: ${(mastery+1)*100} Prestige`;
+
+    const maxPrestigeButton = document.getElementById("maxPrestigeButton");
+    maxPrestigeButton.textContent = `Max Prestige: ` + maxPrestige;
 
     requestAnimationFrame(update);
 }
@@ -67,6 +93,22 @@ function changeTab(event, tabName) {
     event.currentTarget.className += " active";
 }
 
+function maxPrestigeButtonUnlock() {
+    if (!maxPrestigeUnlocked && prestigePoints >= 10) {
+        prestigePoints-=10;
+        maxPrestigeUnlocked = true;
+        document.getElementById("maxPrestigeButton").style.display = "inline";
+    }
+}
+
+function formatNumber(number, decimal = 2) {
+    if (number.toFixed(0).toString().length > 9) {
+        return number.toExponential(decimal);
+    } else {
+        return new Intl.NumberFormat().format(number.toFixed(decimal));
+    }
+}
+
 function handleAutoBlocksPerSecond() {
     if (prestigePoints >= 1) {
         autoBlocksPerSecond+=.1;
@@ -82,11 +124,37 @@ function handleAutoBlockValue() {
 }
 
 function handlePrestige() {
-    if (money >= getPrestigeCost(prestige)) {
-        prestige++;
-        prestigePoints++;
-        prestigeReset();
+    if (maxPrestige) {
+        let values = getMaxPrestige();
+
+        if (values[1] > prestige && values[0] <= money) {
+            prestigePoints += values[1] - prestige;
+            prestige = values[1];
+            prestigeReset();
+        }
+    } else {
+        if (money >= getPrestigeCost(prestige)) {
+            prestige++;
+            prestigePoints++;
+            prestigeReset();
+        }
     }
+}
+
+function getMaxPrestige() {
+    let prestigeNum = prestige + 1;
+    let totalCost = getPrestigeCost(prestige);
+    while (totalCost <= money) {
+        if (money < totalCost + getPrestigeCost(prestigeNum)) break;
+        totalCost += getPrestigeCost(prestigeNum);
+        prestigeNum++;
+    }
+    let values = [];
+    values[0] = totalCost;
+    values[1] = prestigeNum;
+
+
+    return values;
 }
 
 function getPrestigeCost(prestige) {
@@ -112,6 +180,20 @@ function handleTokenFinderUpgrade() {
     if (tokens >= 50) {
         tokens-=50;
         tokenFinder++;
+    }
+}
+
+function handleExplosiveUpgrade() {
+    if (tokens >= 50000 && explosive < 100) {
+        tokens-=50000;
+        explosive++;
+    }
+}
+
+function handleCubicUpgrade() {
+    if (tokens >= 100000 && cubic < 1000) {
+        tokens-=100000;
+        cubic++;
     }
 }
 
@@ -149,6 +231,12 @@ function handleAutoUpgrade() {
     }
 }
 
+function resetButton() {
+    doNotSave = true;
+    localStorage.clear();
+    location.reload();
+}
+
 function resetAll() {
     masteryReset();
     resetEnchantments();
@@ -184,10 +272,36 @@ function backgroundLoop() {
 }
 
 function calcPerTick() {
+    const currentMoney = money;
+    const currentTokens = tokens;
+
     moneyPerBlock+=autoBlockValuePerSecond;
     blocksPerSecond+=autoBlocksPerSecond;
-    increaseMoney(moneyPerBlock * (blocksPerSecond / (1000 / tickInterval)));
-    increaseTokens(blocksPerSecond / (1000 / tickInterval))
+
+    const baseBlocksPerTick = (blocksPerSecond / (1000 / tickInterval));
+    let calcBlocksPerTick = baseBlocksPerTick;
+
+    // Calculate enchantment activation chances and their effect
+    if ((Math.floor(Math.random() * 100) + 1) <= explosive) calcBlocksPerTick+=(baseBlocksPerTick*5);
+    if ((Math.floor(Math.random() * 1000) + 1) <= cubic) calcBlocksPerTick+=(baseBlocksPerTick*25);
+
+    increaseMoney(moneyPerBlock * calcBlocksPerTick);
+    increaseTokens(calcBlocksPerTick);
+
+    averageMoneyPerSec += money - currentMoney;
+    averageTokensPerSec += tokens - currentTokens;
+
+    currentTick++;
+
+    if (currentTick === 10) {
+        currentTick = 1;
+
+        displayMoneyPerSec = averageMoneyPerSec;
+        displayTokensPerSec = averageTokensPerSec;
+
+        averageMoneyPerSec = 0;
+        averageTokensPerSec = 0;
+    }
 }
 
 function load() {
@@ -199,8 +313,8 @@ function load() {
         tokens = parseInt(localStorage.getItem('tokens'));
     }
 
-    if (localStorage.getItem('moneyPerSecond')) {
-        blocksPerSecond = parseInt(localStorage.getItem('moneyPerSecond'));
+    if (localStorage.getItem('blocksPerSecond')) {
+        blocksPerSecond = parseInt(localStorage.getItem('blocksPerSecond'));
     }
 
     if (localStorage.getItem('moneyPerBlock')) {
@@ -231,6 +345,10 @@ function load() {
         tokenFinder = parseInt(localStorage.getItem('tokenFinder'));
     }
 
+    if (localStorage.getItem('explosive')) {
+        explosive = parseInt(localStorage.getItem('explosive'));
+    }
+
     if (localStorage.getItem('mastery')) {
         mastery = parseInt(localStorage.getItem('mastery'));
     }
@@ -239,10 +357,26 @@ function load() {
         masteryPoints = parseInt(localStorage.getItem('masteryPoints'));
     }
 
+    if (localStorage.getItem('cubic')) {
+        cubic = parseInt(localStorage.getItem('cubic'));
+    }
+
+    if (localStorage.getItem('maxPrestige')) {
+        maxPrestige = JSON.parse(localStorage.getItem('maxPrestige'));
+    }
+
+    if (localStorage.getItem('maxPrestigeUnlocked')) {
+        maxPrestigeUnlocked = JSON.parse(localStorage.getItem('maxPrestigeUnlocked'));
+    }
+
+    if (maxPrestigeUnlocked) document.getElementById("maxPrestigeButton").style.display = "inline";
+
     calculateOfflineGain();
 }
 
 function save() {
+    if (doNotSave) return;
+
     localStorage.setItem('money', money);
     localStorage.setItem('tokens', tokens);
     localStorage.setItem('blocksPerSecond', blocksPerSecond);
@@ -255,6 +389,10 @@ function save() {
     localStorage.setItem('tokenFinder', tokenFinder);
     localStorage.setItem('mastery', mastery);
     localStorage.setItem('masteryPoints', masteryPoints);
+    localStorage.setItem('explosive', explosive);
+    localStorage.setItem('cubic', cubic);
+    localStorage.setItem('maxPrestige', maxPrestige);
+    localStorage.setItem('maxPrestigeUnlocked', maxPrestigeUnlocked);
 
     localStorage.setItem('last_save', Date.now());
 }
@@ -272,6 +410,8 @@ function calculateOfflineGain() {
         let gain = money - currentMoney;
         let tokenGain = tokens - currentTokens;
         alert("You have made $" + gain.toFixed(2) + " and " + tokenGain.toFixed(2) + " tokens offline");
+
+
     }
 }
 
