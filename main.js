d11d35e146b7d101d8ff8fe3ea263172f12a98e1
MadCuzDev <30938682+MadCuzDev@users.noreply.github.com>
2023-10-27 04:22:55 -0400

    UI Overhaul, Enchantments
    Redid UI layout and CSS
    Added enchantments

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let money = 0;
+let tokens = 0;
 let moneyPerBlock = 1;
 let blocksPerSecond = 1;
 let tickInterval = 100;
@@ -6,17 +7,45 @@ let prestige = 0;
 let prestigePoints = 0;
 let autoBlocksPerSecond = 0;
 let autoBlockValuePerSecond = 0;
+let fortune = 0;
+let tokenFinder = 0;
 
 function update() {
+    // Update money display
     const moneyDisplay = document.getElementById("money");
-    moneyDisplay.textContent = `$${money.toFixed(2)}\r\nBlocks/Second ${blocksPerSecond.toFixed(1)}\r\n$/Block ${(moneyPerBlock * getMoneyMulti()).toFixed(1)}\r\n$/Second ${(blocksPerSecond * (moneyPerBlock * getMoneyMulti())).toFixed(0)}\r\nPrestige ${prestige}\r\nPrestige Points ${prestigePoints}`;
+    let moneyDisplayText = `$${money.toFixed(2)}`;
+    moneyDisplayText+=`\r\nTokens ${tokens.toFixed(0)}`;
+    moneyDisplayText+=`\r\nTokens/Second ${(blocksPerSecond * getTokenMulti()).toFixed(1)}`;
+    moneyDisplayText+=`\r\nBlocks/Second ${blocksPerSecond.toFixed(1)}`;
+    moneyDisplayText+=`\r\n$/Block ${(moneyPerBlock * getMoneyMulti()).toFixed(1)}`;
+    moneyDisplayText+=`\r\n$/Second ${(blocksPerSecond * (moneyPerBlock * getMoneyMulti())).toFixed(0)}`;
+    moneyDisplayText+=`\r\nPrestige ${prestige}`;
+    moneyDisplayText+=`\r\nPrestige Points ${prestigePoints}`;
 
+    moneyDisplay.textContent = moneyDisplayText;
+
+    // Update enchantment display
+    const enchantmentDisplay = document.getElementById("enchantmentsDisplay");
+    let enchantmentsDisplayText = `Fortune: ${fortune}`;
+    enchantmentsDisplayText+=`\r\nToken Finder: ${tokenFinder}`;
+
+    enchantmentDisplay.textContent = enchantmentsDisplayText;
+
+    // Update prestige display
+    const prestigeDisplay = document.getElementById("prestigeDisplay");
+    let prestigeDisplayText = `auto blocks/sec: ${autoBlocksPerSecond.toFixed(1)}`;
+    prestigeDisplayText+=`\r\n auto block value/sec: ${autoBlockValuePerSecond.toFixed(1)}`
+
+    prestigeDisplay.textContent = prestigeDisplayText;
+
+    // Update prestige button
     const prestigeButton = document.getElementById("prestigeButton");
-    prestigeButton.textContent = `Prestige for ${prestige+2} times money\r\nCost: ${(prestige+1)*100}`;
+    prestigeButton.textContent = `Prestige for ${prestige+2}x money\r\nCost: ${(prestige+1)*100}`;
 
     requestAnimationFrame(update);
 }
 
+// Controls the navigation tabs
 function changeTab(event, tabName) {
     let i, tabs, buttons;
     tabs = document.getElementsByClassName("tab");
@@ -53,12 +82,34 @@ function handlePrestige() {
     }
 }
 
+function handleFortuneUpgrade() {
+    if (tokens >= 20) {
+        tokens-=20;
+        fortune++;
+    }
+}
+
+function handleTokenFinderUpgrade() {
+    if (tokens >= 50) {
+        tokens-=50;
+        tokenFinder++;
+    }
+}
+
 function increaseMoney(amount) {
     money += amount * getMoneyMulti();
 }
 
+function increaseTokens(amount) {
+    tokens += amount * getTokenMulti();
+}
+
 function getMoneyMulti() {
-    return prestige+1;
+    return (prestige+1) * (1+(fortune*0.05));
+}
+
+function getTokenMulti() {
+    return 1 + (tokenFinder * 0.01);
 }
 
 function handleClick() {
@@ -101,11 +152,16 @@ function calcPerTick() {
     moneyPerBlock+=autoBlockValuePerSecond;
     blocksPerSecond+=autoBlocksPerSecond;
     increaseMoney(moneyPerBlock * (blocksPerSecond / (1000 / tickInterval)));
+    increaseTokens(blocksPerSecond / (1000 / tickInterval))
 }
 
 function load() {
     if (localStorage.getItem('money')) {
         money = parseInt(localStorage.getItem('money'));
+    }
+
+    if (localStorage.getItem('tokens')) {
+        tokens = parseInt(localStorage.getItem('tokens'));
     }
 
     if (localStorage.getItem('moneyPerSecond')) {
@@ -132,17 +188,28 @@ function load() {
         autoBlockValuePerSecond = parseFloat(localStorage.getItem('autoBlockValuePerSecond'));
     }
 
+    if (localStorage.getItem('fortune')) {
+        fortune = parseInt(localStorage.getItem('fortune'));
+    }
+
+    if (localStorage.getItem('tokenFinder')) {
+        tokenFinder = parseInt(localStorage.getItem('tokenFinder'));
+    }
+
     calculateOfflineGain();
 }
 
 function save() {
     localStorage.setItem('money', money);
+    localStorage.setItem('tokens', tokens);
     localStorage.setItem('blocksPerSecond', blocksPerSecond);
     localStorage.setItem('moneyPerBlock', moneyPerBlock);
     localStorage.setItem('prestige', prestige);
     localStorage.setItem('prestigePoints', prestigePoints);
     localStorage.setItem('autoBlocksPerSecond', autoBlocksPerSecond);
     localStorage.setItem('autoBlockValuePerSecond', autoBlockValuePerSecond);
+    localStorage.setItem('fortune', fortune);
+    localStorage.setItem('tokenFinder', tokenFinder);
 
     localStorage.setItem('last_save', Date.now());
 }
